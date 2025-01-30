b498dc5739b9d11ec665dc8149e9ebe099e4c67d
Mosè Guerini <m.guerini1@studenti.unipi.it>
2025-01-30 20:08:14 +0100

    Modified load images function

--- utils.py
+++ utils.py
@@ -9,17 +9,19 @@ def load_images(image_path, subdirs):
     images = []
     labels = []
     for sdir in subdirs:
-        if not (pathlib.Path(image_path) / str(sdir)).is_dir():
-               raise FileNotFoundError(f'No such file or directory {pathlib.Path(image_path) / str(sdir)}') 
+        sdir = str(sdir)
+        if not (pathlib.Path(image_path) / sdir).is_dir():
+               raise FileNotFoundError(f'No such file or directory {pathlib.Path(image_path) / sdir}') 
         
-        names = list((pathlib.Path(image_path) / str(sdir)).iterdir())
+        names = list((pathlib.Path(image_path) / sdir).iterdir())
         logger.info(f'Read images from the {sdir} dataset')
-        images += [plt.imread(name) for name in names]
-        labels += [name.stem for name in names]
-    
-    return 
+        images.extend([plt.imread(name) for name in names])
+        labels.extend([name.stem for name in names])
+        
+    return np.array(images, dtype=object), np.array(labels, dtype=np.int32)  #altrimenti np.array(img, dtype=np.float32) / 255.0 se vogliamo normalizzare i pixel tra 0 e 1 per un modello di Machine Learning
 
 def load_labels(labels_path):
     '''Function reading all the labels in a csv file'''
     pass
 
+load_images('/Users/moseguerini/Desktop/Test_dataset', ['Training', 'Validation'])
